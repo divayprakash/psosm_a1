669b5bc8cda64e01ec2cf0fd6be22c4b1d8535c8
Divay Prakash <divayprakash3@gmail.com>
2017-08-19 04:15:49 +0530

    Fixed tweet fetching, hashtag, language and number of tweets now work

--- twitter.py
+++ twitter.py
@@ -17,23 +17,40 @@ else:
 
 searchQuery = '#PanamaPapers'
 maxTweets = 10000
+tweetsPerQry = 100
 fName = 'tweets.txt'
+language = 'en'
+
+max_id = -1L
 
 tweetCount = 0
 print("Downloading max {0} tweets".format(maxTweets))
 
-tweets_list = []
+tweets_list_lowercase = []
 
 with open(fName, 'w') as f:
-  for tweet in tweepy.Cursor(api.search,q=searchQuery,iso_language_code="en").items(maxTweets) :
-    obj = jsonpickle.encode(tweet._json, unpicklable=False)
-    data = json.loads(obj)
-    text = data['text'].encode('utf-8')
-    text = str.lower(text)
-    if text[0:2] != 'rt' and text not in tweets_list:
-      tweets_list.append(text)
-      f.write(obj + '\n')
-      tweetCount += 1
+  while tweetCount < maxTweets:
+    try:
+      if (max_id <= 0):
+        new_tweets = api.search(q=searchQuery, lang=language, count=tweetsPerQry)
+      else:
+        new_tweets = api.search(q=searchQuery, lang=language, count=tweetsPerQry, max_id=str(max_id - 1))
+      if not new_tweets:
+        print("No more tweets found!")
+        break
+      for tweet in new_tweets:
+        obj = jsonpickle.encode(tweet._json, unpicklable=False)
+        data = json.loads(obj)
+        text = data['text'].encode('utf-8')
+        text = str.lower(text)
+        if text[0:2] != 'rt' and text not in tweets_list_lowercase:
+          tweets_list_lowercase.append(text)
+          f.write(obj + '\n')
+          tweetCount += 1
       print("Downloaded {0} tweets".format(tweetCount))
+      max_id = new_tweets[-1].id
+    except tweepy.TweepError as e:
+      print("some error : " + str(e))
+      break
 
 print ("Downloaded {0} tweets, saved to {1}".format(tweetCount, fName))
