8f443814240336f2b050c7700b2f8f7f1bec3671
Divay Prakash <divayprakash3@gmail.com>
2017-08-19 17:08:52 +0530

    Bonus question done!

---
+++ bonus.py
@@ -0,0 +1,22 @@
+########## IMPORTS ##########
+import config  # external configuration file with API keys
+import tweepy
+
+########## CONNECTING TO TWITTER API USING TWEEPY ##########
+auth = tweepy.AppAuthHandler(config.access['API_KEY'], config.access['API_SECRET'])
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+if (not api):
+  print ("Twitter API authentication failed")
+  sys.exit(-1)
+
+########## DEFINE VARIABLES ##########
+user = "@divayprakash3"
+tweets = []
+
+########## GET ALL TWEETS OF USER ##########
+for tweet in tweepy.Cursor(api.user_timeline, id=user).items():
+  tweets.append(tweet)
+
+########## PRINT FIRST TWEET ##########
+print tweets[-1].text
