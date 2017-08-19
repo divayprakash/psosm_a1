dc8a8891d0b4e5189c94525a3e8fbf610c896098
Divay Prakash <divayprakash3@gmail.com>
2017-08-19 20:42:48 +0530

    Refactor

--- facebook.py
+++ facebook.py
@@ -1,4 +1,5 @@
-import config
+########## IMPORTS ##########
+import config  # external configuration file with API keys
 import json
 import facebook
 from requests_oauthlib import OAuth2Session
@@ -7,20 +8,15 @@ from requests_oauthlib.compliance_fixes import facebook_compliance_fix
 client_id = config.facebook['client_id']
 client_secret = config.facebook['client_secret']
 
-base_url = 'https://www.facebook.com/dialog/oauth'
-token_url = 'https://graph.facebook.com/oauth/access_token'
-
-redirect_uri = 'https://divayprakash.com/'
-scope = ('user_birthday', 'user_hometown', 'user_likes')
-fb = OAuth2Session(client_id, redirect_uri=redirect_uri, scope=scope)
+fb = OAuth2Session(client_id, redirect_uri='https://divayprakash.com/', scope=('user_birthday', 'user_hometown', 'user_likes'))
 fb = facebook_compliance_fix(fb)
 
-authorization_url, state = fb.authorization_url(base_url)
+authorization_url, state = fb.authorization_url('https://www.facebook.com/dialog/oauth')
 print 'Please authorize here: ', authorization_url
 
-redirect_response = raw_input('Please paste the full redirect URL here: ')
-result = fb.fetch_token(token_url, client_secret=client_secret, authorization_response=redirect_response)
+redirect = raw_input('Redirect URL here: ')
+result = fb.fetch_token('https://graph.facebook.com/oauth/access_token', client_secret=client_secret, authorization_response=redirect)
 
-temp = fb.get("https://graph.facebook.com/me?fields=id,name,birthday,languages,hometown")
-data = json.loads(temp.content)
+unset = fb.get("https://graph.facebook.com/me?fields=id,name,birthday,languages,hometown")
+data = json.loads(unset.content)
 print data
