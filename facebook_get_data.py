7e8823a9f0f7c1fc40f151e89bbb1d07d485b8aa
Divay Prakash <divayprakash3@gmail.com>
2017-08-19 20:51:02 +0530

    Rename file and add documentation

--- facebook_get_data.py
+++ facebook_get_data.py
@@ -5,18 +5,24 @@ import facebook
 from requests_oauthlib import OAuth2Session
 from requests_oauthlib.compliance_fixes import facebook_compliance_fix
 
+########## GET CONFIG FROM FILE ##########
 client_id = config.facebook['client_id']
 client_secret = config.facebook['client_secret']
 
+########## INITIALIZE SESSION ##########
 fb = OAuth2Session(client_id, redirect_uri='https://divayprakash.com/', scope=('user_birthday', 'user_hometown', 'user_likes'))
 fb = facebook_compliance_fix(fb)
 
 authorization_url, state = fb.authorization_url('https://www.facebook.com/dialog/oauth')
 print 'Please authorize here: ', authorization_url
 
+########## GET REDIRECT URL AND RETREIVE ACCESS TOKEN ##########
 redirect = raw_input('Redirect URL here: ')
 result = fb.fetch_token('https://graph.facebook.com/oauth/access_token', client_secret=client_secret, authorization_response=redirect)
 
+########## GET DATA FROM ACCESS TOKEN ##########
 unset = fb.get("https://graph.facebook.com/me?fields=id,name,birthday,languages,hometown")
 data = json.loads(unset.content)
+
+########## CONSOLE LOG DATA ##########
 print data
